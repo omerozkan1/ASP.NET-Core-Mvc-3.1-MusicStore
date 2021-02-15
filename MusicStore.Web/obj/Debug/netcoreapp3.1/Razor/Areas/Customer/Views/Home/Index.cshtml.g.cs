d12c05d1ba01b796ccc430d47b6a6d3068b0647a
omer <gs_omer_ozkan@hotmail.com>
2021-02-16 00:13:05 +0300

    css clothed and database created

--- MusicStore.Web/obj/Debug/netcoreapp3.1/Razor/Areas/Customer/Views/Home/Index.cshtml.g.cs
+++ MusicStore.Web/obj/Debug/netcoreapp3.1/Razor/Areas/Customer/Views/Home/Index.cshtml.g.cs
@@ -13,6 +13,7 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"99464617055fdb505bb0bb7fd91f9b14f7b0d030", @"/Areas/Customer/Views/Home/Index.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"57218c316b6921e2cd61027a2387edc31a2d9471", @"/Areas/Customer/Views/_ViewImports.cshtml")]
     public class Areas_Customer_Views_Home_Index : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
