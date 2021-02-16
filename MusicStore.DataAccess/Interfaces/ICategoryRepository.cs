cc469cd94b6316ea45275581bb3d3691a885bbda
omer <gs_omer_ozkan@hotmail.com>
2021-02-17 00:56:03 +0300

    category and spcall repository added

---
+++ MusicStore.DataAccess/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using MusicStore.Models.DbModels;
+
+namespace MusicStore.DataAccess.Interfaces
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
